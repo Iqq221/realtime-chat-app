5a6351b926372327194e1d6375ba0b2615109e84
iqq221 <iqravadnagar2211@gmail.com>
2026-07-20 20:21:06 +0530

    feat: configure backend server and connect MongoDB Atlas

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const connectDB = require("./src/config/db");
+
+dotenv.config();
+
+// Connect Database
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+// Test Route
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Realtime Chat API is running 🚀",
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
